97b72b65e60d7d874934c731f93ecbe25d022adb
rutvi <rutvirathod423@gmail.com>
2025-11-24 19:28:56 -0500

    added projects and image

--- src/componants/projects/projects.component.ts
+++ src/componants/projects/projects.component.ts
@@ -25,23 +25,43 @@ export class ProjectsComponent implements OnInit {
         name: 'Azure'},{
         name: 'Third party APIs'
         }]
-    }
-    // {
-    //   title: 'Project Two',
-    //   description: 'Description for project two',
-    //   imageUrl: '../../assets/appoint-meet.png',
-    //   technology: [{
-    //     name: 'Angular'},{
-    //     name: 'Typescript'},{
-    //       name: 'HTML'},{
-    //       name: 'CSS'},{
-    //       name: 'Javascript'},{
-    //       name: 'Mongodb'},{
-    //       name: 'MySQL'},{
-    //       name: 'Git'},{
-    //       name: 'Postman'}]
+    },
+    {
+      title: 'Mini DNS API',
+      description: 'A Mini DNS Server is a lightweight application that resolves domain names to IP addresses within a local network.'+ 
+      'It handles DNS queries, supports basic record types (A, CNAME, MX), and provides caching for improved performance.' +
+      'Ideal for small-scale environments, it enhances network efficiency and reliability by managing local DNS resolution.',
+      imageUrl: '../../assets/DNS API image.png',
+      technology: [{
+        name: 'Typescript'},{
+          name: 'HTML'},{
+          name: 'CSS'},{
+          name: 'Javascript'},{
+          name: 'NodeJs'},{
+          name: 'MySQL'},{
+          name: 'Git'},{
+          name: 'Postman'}]
         
-    // }
+    },
+    {
+      title: 'Career Cloud',
+      description: 'A Career Cloud is an online platform that connects job seekers with employers, offering features like job listings and application tracking.' +
+      'It streamlines efficient job search and recruitment processes.' +
+      'Ideal for individuals seeking career growth and organizations looking to attract talent.',
+      imageUrl: '../../assets/Careercloud image.png',
+      technology: [{
+        name: '.net'},{
+          name: 'MVC'},{
+          name: 'Entity Framework',},{
+          name: 'HTML'},{
+          name: 'CSS'},{
+          name: 'Javascript'},{
+          name: 'C#'},{
+          name: 'LINQ'},{
+          name: 'Git'},{
+          name: 'Microsoft SQL Server'}]
+        
+    }
   ];
   ;
 
